c6da8a45c375ac69b8b1ae1018c1f09a35829f3e
Forde Nedimovic <forde@dal.ca>
2024-02-08 11:26:01 -0400

    work on the crusie list page is compleate

---
+++ javascript/cruiseList.js
@@ -0,0 +1,18 @@
+let fakeData = ["Condor", "Bluenose", "Kingston", "Atlantica" , "Offshore New Zealand, R/V Tangaroa"];
+let fakeDataTwo = [3456788, 252352, 2525235, 242342342 , 24234242];
+
+        let list = document.getElementById("clItems");
+        for (i = 0; i < fakeData.length; ++i) {
+            let li = document.createElement('li');
+            li.innerText = "NAME: " + fakeData[i] + " " + "ID:" + " " + fakeDataTwo[i] + " " ;
+            list.appendChild(li);
+            
+            let button = document.createElement('button');
+            button.innerText = "View Logs"; 
+            li.appendChild(button);
+
+
+            let button2 = document.createElement('button');
+            button2.innerText = "Exsport Data"; 
+            li.appendChild(button2);
+        }
