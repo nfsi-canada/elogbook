cc319a44d324f3beebcc863b408ce9d7fc9ac84f
Forde Nedimovic <forde@dal.ca>
2024-02-20 16:47:07 -0400

    massive work on the logs page

--- javascript/logs.js
+++ javascript/logs.js
@@ -10,50 +10,54 @@ let cID = 3456788; // sent id
 
 let fakeDataBase = [
     
-    [0, 1, 2, 3 , 4, 5],
-    ["Log 1", "Log 2", "Log 3", "Log 4" , "Log 5", "Log 6"],
-    ["Mladen", "Katie", "Gramm", "Jon", "Forde", "phil"],   
-    ["16:46","23:46","01:46","6:16","09:06","11:57"], 
-    ["03/01/2023","03/07/2024","05/01/2023","11/05/2023","12/12/2023","30/12/2025"],
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+    ["2023-12-20","2024-01-02","2024-1-20","2024-02-02","2024-02-19","2024-02-20", "2023-12-20","2024-01-02","2024-1-20","2024-02-02","2024-02-19","2024-02-20"],
+    ["16:46","23:46","01:46","6:16","09:06","11:57","16:46","23:46","01:46","6:16","09:06","11:57"], 
+    ["DEPLOYMENT", "RECOVERY", "DEPLOYMENT", "COMMENTS" , "RECOVERY", "DEPLOYMENT","DEPLOYMENT", "RECOVERY", "DEPLOYMENT", "COMMENTS" , "RECOVERY", "DEPLOYMENT"],
     ["some stuff happend",
     "nothing happend",
     "this is some crazy da da right here",
     "there once was a ship that put to sea, the name of the ship was a..",
     "billy o tea, some say the wellerman comes to...",
-    "bring us sugar tea and rum, this of course being a referance to a well known sea shanty that I do enjoy, i mean even with my bad spelling I hope to properly convay that idea, this is ment to be a long text in a  log entry..."]                     
+    "bring us sugar tea and rum, this of course being a referance to a well known sea shanty that...",
+    "some stuff happend",
+    "nothing happend",
+    "this is some crazy da da right here",
+    "there once was a ship that put to sea, the name of the ship was a..",
+    "billy o tea, some say the wellerman comes to...",
+    "bring us sugar tea and rum, this of course being a referance to a well known sea shanty that I happen to like, da da da tah da da da, da da, da da da da, good song true story, da da da dum da da dum dum da dum..."],
+    ["Mladen", "Katie", "Gramm", "Jon", "Forde", "phil","Mladen", "Katie", "Gramm", "Jon", "Forde", "phil"]                    
 ]
 
 let list = document.getElementById("logs");
 
-for (i = 0; i < fakeDataBase.length; i++) {             // i = 1 beacuse we dont creat any elements for the ID
+for (i = 0; i < fakeDataBase[1].length; i++) {            
 
-    let lTitle = document.createElement('li');
-    lTitle.innerText = fakeDataBase[1][i];
-    list.appendChild(lTitle);
-
-    let br = document.createElement('br');
-    lTitle.appendChild(br);
-
-
+    let tr = document.createElement('tr');
     
+    for (j = 1; j < fakeDataBase.length; j++) {         // i = 2 beacuse we skip id 
+
+        let li = document.createElement('th');
+        li.innerText = fakeDataBase[j][i];
+        tr.appendChild(li);
+        list.appendChild(tr);
+    }
+    let viewData = document.createElement('th');
     let link = document.createElement('a');
     link.setAttribute('href',"log.html");
     let button = document.createElement('button');
-    button.innerText = "View Full Log"; 
+    button.innerText = "View"; 
     link.appendChild(button);
-    lTitle.appendChild(link);
+    viewData.appendChild(link);
+    tr.appendChild(viewData);
 
-
-    for (j = 2; j < fakeDataBase.length; j++) {         // i = 2 beacuse we skip id and log number and display info
-
-        let ul = document.createElement('ul');
-    
-        let li = document.createElement('li');
-        li.innerText = fakeDataBase[j][i];
-        ul.appendChild(li);
-
-        list.appendChild(ul);
-    }
-    
+    let editData = document.createElement('th');
+    let linkTwo = document.createElement('a');
+    linkTwo.setAttribute('href',"TBD.html");
+    let buttonTwo = document.createElement('button');
+    buttonTwo.innerText = "Edit"; 
+    linkTwo.appendChild(buttonTwo);
+    editData.appendChild(linkTwo);
+    tr.appendChild(editData);
 }
 
