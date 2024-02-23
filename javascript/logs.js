cf87b516bb3ee76ad6c89f4f11549e7ad1726617
Forde Nedimovic <forde@dal.ca>
2024-02-23 14:24:25 -0400

    further changes and updates to the log list page and the crusie list page

--- javascript/logs.js
+++ javascript/logs.js
@@ -10,10 +10,10 @@ let cID = 3456788; // sent id
 
 let fakeDataBase = [
     
-    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
-    ["2023-12-20","2024-01-02","2024-1-20","2024-02-02","2024-02-19","2024-02-20", "2023-12-20","2024-01-02","2024-1-20","2024-02-02","2024-02-19","2024-02-20"],
-    ["16:46","23:46","01:46","6:16","09:06","11:57","16:46","23:46","01:46","6:16","09:06","11:57"], 
-    ["DEPLOYMENT", "RECOVERY", "DEPLOYMENT", "COMMENTS" , "RECOVERY", "DEPLOYMENT","DEPLOYMENT", "RECOVERY", "DEPLOYMENT", "COMMENTS" , "RECOVERY", "DEPLOYMENT"],
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23],
+    ["2023-12-20","2024-01-02","2024-1-20","2024-02-02","2024-02-19","2024-02-20", "2023-12-20","2024-01-02","2024-1-20","2024-02-02","2024-02-19","2024-02-20","2023-12-20","2024-01-02","2024-1-20","2024-02-02","2024-02-19","2024-02-20", "2023-12-20","2024-01-02","2024-1-20","2024-02-02","2024-02-19","2024-02-20"],
+    ["16:46","23:46","01:46","6:16","09:06","11:57","16:46","23:46","01:46","6:16","09:06","11:57","16:46","23:46","01:46","6:16","09:06","11:57","16:46","23:46","01:46","6:16","09:06","11:57"], 
+    ["DEPLOYMENT", "RECOVERY", "DEPLOYMENT", "COMMENTS" , "RECOVERY", "DEPLOYMENT","DEPLOYMENT", "RECOVERY", "DEPLOYMENT", "COMMENTS" , "RECOVERY", "DEPLOYMENT","DEPLOYMENT", "RECOVERY", "DEPLOYMENT", "COMMENTS" , "RECOVERY", "DEPLOYMENT","DEPLOYMENT", "RECOVERY", "DEPLOYMENT", "COMMENTS" , "RECOVERY", "DEPLOYMENT"],
     ["some stuff happend",
     "nothing happend",
     "this is some crazy da da right here",
@@ -25,8 +25,20 @@ let fakeDataBase = [
     "this is some crazy da da right here",
     "there once was a ship that put to sea, the name of the ship was a..",
     "billy o tea, some say the wellerman comes to...",
+    "bring us sugar tea and rum, this of course being a referance to a well known sea shanty that I happen to like, da da da tah da da da, da da, da da da da, good song true story, da da da dum da da dum dum da dum...",
+    "some stuff happend",
+    "nothing happend",
+    "this is some crazy da da right here",
+    "there once was a ship that put to sea, the name of the ship was a..",
+    "billy o tea, some say the wellerman comes to...",
+    "bring us sugar tea and rum, this of course being a referance to a well known sea shanty that...",
+    "some stuff happend",
+    "nothing happend",
+    "this is some crazy da da right here",
+    "there once was a ship that put to sea, the name of the ship was a..",
+    "billy o tea, some say the wellerman comes to...",
     "bring us sugar tea and rum, this of course being a referance to a well known sea shanty that I happen to like, da da da tah da da da, da da, da da da da, good song true story, da da da dum da da dum dum da dum..."],
-    ["Mladen", "Katie", "Gramm", "Jon", "Forde", "phil","Mladen", "Katie", "Gramm", "Jon", "Forde", "phil"]                    
+    ["Mladen", "Katie", "Gramm", "Jon", "Forde", "phil","Mladen", "Katie", "Gramm", "Jon", "Forde", "phil","Mladen", "Katie", "Gramm", "Jon", "Forde", "phil","Mladen", "Katie", "Gramm", "Jon", "Forde", "phil"]                    
 ]
 
 let list = document.getElementById("logs");
